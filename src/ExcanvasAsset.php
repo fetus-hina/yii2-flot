b3603ac34b67aec21d5ed0bf740fe0ec7ca13567
AIZAWA, Hina <hina@bouhime.com>
2015-12-25 21:02:50 +0900

    add assets

---
+++ src/ExcanvasAsset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015 AIZAWA Hina
+ * @license https://github.com/fetus-hina/yii2-flot/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+namespace jp3cki\yii2\flot;
+
+use yii\web\AssetBundle;
+
+class ExcanvasAsset extends AssetBundle
+{
+    public $sourcePath = '@bower/flot';
+    public $js = [
+        'excanvas.js',
+    ];
+    public $jsOptions = [
+        'condition' => 'lte IE 8',
+    ];
+}
---
+++ src/FlotPieAsset.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright Copyright (C) 2015 AIZAWA Hina
+ * @license https://github.com/fetus-hina/yii2-flot/blob/master/LICENSE MIT
+ * @author AIZAWA Hina <hina@bouhime.com>
+ */
+
+namespace jp3cki\yii2\flot;
+
+class FlotPieAsset extends BasePluginAsset
+{
+    public $js = [
+        'jquery.flot.pie.js',
+    ];
+}
